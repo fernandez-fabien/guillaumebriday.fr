4936634fb3e6a5a813c0bfaef9ad12d9405609a3
Guillaume Briday <guillaumebriday@gmail.com>
2017-07-03 13:33:44 +0200

    Removing baguetteBox

--- _javascripts/baguetteBox.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("turbolinks:load", function() {
-  var baguetteBox = require('../node_modules/baguettebox.js/dist/baguetteBox.js');
-
-  baguetteBox.run('.gallery-grid', {
-    animation: false,
-    overlayBackgroundColor: 'rgba(0,0,0,0.9)',
-  });
-});
