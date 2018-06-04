70bb8ff56d8bb1d2d79681cdd307a022a363f785
Guillaume Briday <guillaumebriday@gmail.com>
2018-06-04 10:03:16 +0200

    Updating colors from red to indigo

--- assets/javascripts/components/CommentForm.vue
+++ assets/javascripts/components/CommentForm.vue
@@ -46,10 +46,12 @@
         <em>Les commentaires sont formatés en <a href="https://daringfireball.net/projects/markdown/syntax/">markdown</a>.</em>
       </div>
 
-      <button v-if="!isPosted" :disabled="isDisabled" :class="{ 'opacity-50 cursor-not-allowed': isDisabled }" class="bg-white border border-red hover:bg-red hover:text-white cursor-pointer text-red shadow w-full py-2 px-4 rounded" type="submit">
-        <i v-if="isLoading" class="fa fa-spinner fa-spin fa-fw"></i>
-        Laisser un commentaire
-      </button>
+      <div v-if="!isPosted" :disabled="isDisabled" class="flex justify-end">
+        <button :class="{ 'opacity-50 cursor-not-allowed': isDisabled }" class="bg-white border border-indigo hover:bg-indigo hover:text-white cursor-pointer text-indigo shadow py-2 px-4 rounded w-full sm:w-auto" type="submit">
+          <i v-if="isLoading" class="fa fa-spinner fa-spin fa-fw"></i>
+          Laisser un commentaire
+        </button>
+      </div>
     </form>
   </div>
 </template>
